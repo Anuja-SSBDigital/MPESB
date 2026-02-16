cb5defddffd6ede0694c6eb45924a756919c40eb
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2026-02-16 12:28:52 +0530

    add hide and show in viewbage in home cont

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -10,48 +10,56 @@ namespace MPESB.Controllers
         public ActionResult Index()
         {
             ViewBag.Title = "Home";
+            ViewBag.ShowSidebar = true;
             return View();
         }
 
         public ActionResult About()
         {
             ViewBag.Title = "About Us";
+            ViewBag.ShowSidebar = true;
             return View();
         }
 
         public ActionResult Dashboard()
         {
             ViewBag.Title = "Dashboard";
+            ViewBag.ShowSidebar = false;
             return View();
         }
 
         public ActionResult Mission()
         {
             ViewBag.Title = "Mission & Vision";
+            ViewBag.ShowSidebar = false;
             return View();
         }
 
         public ActionResult OnlineForm()
         {
             ViewBag.Title = "Online Form";
+            ViewBag.ShowSidebar = false;
             return View();
         }
 
         public ActionResult AdmitCard()
         {
             ViewBag.Title = "Admit Card";
+            ViewBag.ShowSidebar = false;
             return View();
         }
 
         public ActionResult Results()
         {
             ViewBag.Title = "Results";
+            ViewBag.ShowSidebar = false;
             return View();
         }
 
         public ActionResult Contact()
         {
             ViewBag.Title = "Contact Us";
+            ViewBag.ShowSidebar = false;
             return View();
         }
     }
